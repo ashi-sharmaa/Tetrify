c37d91b0ddf8b6135c25c37edaf838cad6ea6a76
R. A. Pratama <rapratama2005@gmail.com>
2024-11-02 18:21:08 -0500

    Update handtracker.py

--- handtracker.py
+++ handtracker.py
@@ -3,12 +3,17 @@
 #2.OpenCV
 import cv2
 import mediapipe as mp
+from enum import Enum
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+# Stores the different positions
+Direction = Enum('Direction', [('UP', 1), ('DOWN', 2), ('LEFT', 3), ('RIGHT', 4), ('NEUTRAL', 5)])
+
 # Gets the average position
-def palmAvg(points):
+def avg2tuple(points) -> tuple:
   avg_x = 0.0
   avg_y = 0.0
   for point in points:
@@ -18,26 +23,30 @@ def palmAvg(points):
   avg_x /= len(points)
   avg_y /= len(points)
   
-  print('average x coord: ' + str(avg_x) + ' --- average y coord: ' + str(avg_y))
+  # print('average x coord: ' + str(avg_x) + ' --- average y coord: ' + str(avg_y))
+  return [avg_x, avg_y]
 
-def avgPalmPos(palms):
-  avg_x = 0.0
-  avg_y = 0.0
-  for point in palms:
-    avg_x += point[0]
-    avg_y += point[1]
-  
-  avg_x /= len(palms)
-  avg_y /= len(palms)
-  
-  print('average x coord: ' + str(avg_x) + ' --- average y coord: ' + str(avg_y))
-
-
-
+timems = 0
+palms = []
 cap = cv2.VideoCapture(0)
 hands=mp_hands.Hands()
 print_once = 1 == 1
-while cap.isOpened():
+
+rTop = 0.25
+rBottom = 0.25
+rRight = 0.15
+rLeft = 0.15
+
+cTop = 0
+cBottom = 0
+cRight = 0
+cLeft = 0
+
+center = [2]
+curPos = [2]
+
+currentDirection = Direction.NEUTRAL
+while timems > -1:
     success, image = cap.read()
     # Flip the image horizontally for a later selfie-view display, and convert
     # the BGR image to RGB.
@@ -51,7 +60,8 @@ while cap.isOpened():
     if results.multi_hand_landmarks:
       #prints to terminal coordinates
       #0, 1, 5, 9, 13, 17
-      if print_once:
+      if timems > 50 and timems < 100: 
+        print('Getting avgs')
         palm_indexes = [0, 1, 5, 9, 13, 17]
         palm_coordinates = []
         for hand_landmarks in results.multi_hand_landmarks:
@@ -66,14 +76,52 @@ while cap.isOpened():
               landmark_index = landmark_index + 1
         print(palm_coordinates)
         
-        palmAvg(palm_coordinates)                 
+        palms.append(avg2tuple(palm_coordinates))
+        
+      if timems == 120:
+        print('output pos')
+        center = avg2tuple(palms)
+        print(center)
+        cTop = center[1] - rTop
+        cBottom = center[1] + rBottom
+        cRight = center[0] + rRight
+        cLeft = center[0] - rLeft
+        
 
-
-      # for index in palm_indexes:
-      #   print(index)
-      #   print(results.multi_hand_landmarks[0])
-      print_once = 1==0
-
+      if timems > 150:
+        palm_indexes = [0, 1, 5, 9, 13, 17]
+        palm_coordinates = []
+        for hand_landmarks in results.multi_hand_landmarks:
+           # Here is getting coordinates
+          landmark_index = 0
+          arr_index = 0
+          for ids, landmrk in enumerate(hand_landmarks.landmark):
+              if landmark_index in palm_indexes:
+                #add to the list of relevant coordinates
+                palm_coordinates.append([landmrk.x, landmrk.y])
+                arr_index = arr_index + 1
+              landmark_index = landmark_index + 1
+        # print(palm_coordinates)
+        
+        curPos = avg2tuple(palm_coordinates)
+        
+        if curPos[1] < cTop and currentDirection != Direction.UP: 
+          print('Top')
+          currentDirection = Direction.UP
+        elif curPos[1] > cBottom and currentDirection != Direction.DOWN: 
+          print('Bottom')
+          currentDirection = Direction.DOWN
+        elif curPos[0] < cLeft and currentDirection != Direction.LEFT: 
+          print('Left')
+          currentDirection = Direction.LEFT
+        elif curPos[0] > cRight and currentDirection != Direction.RIGHT: 
+          print('Right')
+          currentDirection = Direction.RIGHT
+        elif not(curPos[1] < cTop or curPos[1] > cBottom or curPos[0] < cLeft or curPos[0] > cRight):
+          #no direction condition is passed
+          currentDirection = Direction.NEUTRAL
+        
+      
       #displays joints
       for hand_landmarks in results.multi_hand_landmarks:                                                                         
         mp_drawing.draw_landmarks(
@@ -81,6 +129,9 @@ while cap.isOpened():
             hand_landmarks,mp_hands.HAND_CONNECTIONS)
     
     cv2.imshow('MediaPipe Hands', image)
+    timems = timems + 1
+    if timems < 150:
+      print(str(timems))
     cv2.waitKey(1)
 
 cap.release()
