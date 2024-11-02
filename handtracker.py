b3369721a32c84f263cd4b0f08e8dab9a71946d4
R. A. Pratama <rapratama2005@gmail.com>
2024-11-02 15:40:07 -0500

    asd

--- handtracker.py
+++ handtracker.py
@@ -9,7 +9,8 @@ mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
 hands=mp_hands.Hands()
-while True:
+print_once = 1 == 1
+while cap.isOpened():
     success, image = cap.read()
     # Flip the image horizontally for a later selfie-view display, and convert
     # the BGR image to RGB.
@@ -18,10 +19,52 @@ while True:
     # pass by reference.
     results = hands.process(image)
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+
+    #renders results
     if results.multi_hand_landmarks:
+      #prints to terminal coordinates
+      #0, 1, 5, 9, 13, 17
+      if print_once:
+        palm_indexes = [0, 1, 5, 9, 13, 17]
+        palm_coordinates = []
+        for hand_landmarks in results.multi_hand_landmarks:
+           # Here is getting coordinates
+          landmark_index = 0
+          arr_index = 0
+          for ids, landmrk in enumerate(hand_landmarks.landmark):
+              if landmark_index in palm_indexes:
+                #add to the list of relevant coordinates
+                palm_coordinates.append([landmrk.x, landmrk.y])
+                arr_index = arr_index + 1
+              landmark_index = landmark_index + 1
+        print(palm_coordinates)
+        
+        avg_x = 0.0
+        avg_y = 0.0
+        for point in palm_coordinates:
+          avg_x += point[0]
+          avg_y += point[1]
+        
+        avg_x /= len(palm_coordinates)
+        avg_y /= len(palm_coordinates)
+        
+      print('average x coord: ' + avg_x + ' --- average y coord: ' + avg_y)
+                 
+
+
+      # for index in palm_indexes:
+      #   print(index)
+      #   print(results.multi_hand_landmarks[0])
+      print_once = 1==0
+
+      #displays joints
       for hand_landmarks in results.multi_hand_landmarks:                                                                         
         mp_drawing.draw_landmarks(
             image,
             hand_landmarks,mp_hands.HAND_CONNECTIONS)
+    
     cv2.imshow('MediaPipe Hands', image)
     cv2.waitKey(1)
+
+cap.release()
+cv2.destroyAllWindows()
