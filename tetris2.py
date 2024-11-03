bb79ec3393b7387bfae901954e7286e8ee8c70e6
R. A. Pratama <rapratama2005@gmail.com>
2024-11-02 23:17:16 -0500

    gack

--- tetris2.py
+++ tetris2.py
@@ -909,84 +909,38 @@ def gameLoop():
                     landmark_index = landmark_index + 1
 
             curPos = avg2tuple(palm_coordinates)
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             
             if curPos[1] < cTop and currentDirection != Direction.UP: 
-                print('Top')
-                currentDirection = Direction.UP
+                if key.rotate.status is 'idle':
+                  key.rotate.trig = True
+                  key.rotate.status = 'pressed'
+                  print('Top')
+                  currentDirection = Direction.UP
             elif curPos[1] > cBottom and currentDirection != Direction.DOWN: 
+                key.down.status = 'pressed'
                 print('Bottom')
                 currentDirection = Direction.DOWN
-            elif curPos[0] < cLeft and currentDirection != Direction.LEFT: 
+            elif curPos[0] < cLeft and currentDirection != Direction.LEFT:
+                xChange += -1
                 print('Left')
                 currentDirection = Direction.LEFT
             elif curPos[0] > cRight and currentDirection != Direction.RIGHT: 
+                xChange += 1
                 print('Right')
                 currentDirection = Direction.RIGHT
             elif not(curPos[1] < cTop or curPos[1] > cBottom or curPos[0] < cLeft or curPos[0] > cRight):
             #no direction condition is passed
-                currentDirection = Direction.NEUTRAL
-
-        for event in pygame.event.get():	
-            if event.type == pygame.QUIT: #Looks for quitting event in every iteration (Meaning closing the game window)
-                gameExit = True
-                
-            if event.type == pygame.KEYDOWN: #Keyboard keys press events
-                if event.key == pygame.K_LEFT:
-                    xChange += -1
-                if event.key == pygame.K_RIGHT:
-                    xChange += 1
-                if event.key == pygame.K_DOWN:
-                    key.down.status = 'pressed'
-                if event.key == pygame.K_UP:
-                    if key.rotate.status == 'idle':
-                        key.rotate.trig = True
-                        key.rotate.status = 'pressed'
-                if event.key == pygame.K_z:
-                    if key.cRotate.status == 'idle':
-                        key.cRotate.trig = True
-                        key.cRotate.status = 'pressed'
-                if event.key == pygame.K_p:
-                    if key.pause.status == 'idle':
-                        key.pause.trig = True
-                        key.pause.status = 'pressed'
-                if event.key == pygame.K_r:
-                    if key.restart.status == 'idle':
-                        key.restart.trig = True
-                        key.restart.status = 'pressed'
-                if event.key == pygame.K_RETURN:
-                    key.enter.status = 'pressed'
-                        
-            if event.type == pygame.KEYUP: #Keyboard keys release events
-                if event.key == pygame.K_LEFT:
-                    xChange += 1
-                if event.key == pygame.K_RIGHT:
-                    xChange += -1
-                if event.key == pygame.K_DOWN:
-                    key.down.status = 'released'
-                if event.key == pygame.K_UP:
-                    key.rotate.status = 'idle'
-                if event.key == pygame.K_z:
-                    key.cRotate.status = 'idle'
-                if event.key == pygame.K_p:
-                    key.pause.status = 'idle'
-                if event.key == pygame.K_r:
-                    key.restart.status = 'idle'
-                if event.key == pygame.K_RETURN:
-                    key.enter.status = 'idle'
-            
-            if currentDirection == Direction.UP:
-                key.rotate.status = 'pressed'
-            elif currentDirection == Direction.LEFT:
-                xChange += -1
-            elif currentDirection == Direction.RIGHT:
-                xChange += 1
-            elif currentDirection == Direction.DOWN:
                 key.down.status = 'pressed'
-            else:
-                # neutral
+                currentDirection = Direction.NEUTRAL
                 key.rotate.status = 'idle'
                 key.down.status = 'released'
                 xChange = 0
+            else:
+                key.down.status = 'pressed'
+                key.rotate.status = 'idle'
+                key.down.status = 'released'
             
             if xChange > 0:
                 key.xNav.status = 'right'
@@ -994,13 +948,62 @@ def gameLoop():
                 key.xNav.status = 'left'	
             else:
                 key.xNav.status = 'idle'
+
+        for event in pygame.event.get():	
+            # if event.type == pygame.QUIT: #Looks for quitting event in every iteration (Meaning closing the game window)
+            #     gameExit = True
+                
+            if event.type == pygame.KEYDOWN: #Keyboard keys press events
+            #     if event.key == pygame.K_LEFT:
+            #         xChange += -1
+            #     if event.key == pygame.K_RIGHT:
+            #         xChange += 1
+            #     if event.key == pygame.K_DOWN:
+            #         key.down.status = 'pressed'
+            #     if event.key == pygame.K_UP:
+            #         if key.rotate.status == 'idle':
+            #             key.rotate.trig = True
+            #             key.rotate.status = 'pressed'
+            #     if event.key == pygame.K_z:
+            #         if key.cRotate.status == 'idle':
+            #             key.cRotate.trig = True
+            #             key.cRotate.status = 'pressed'
+            #     if event.key == pygame.K_p:
+            #         if key.pause.status == 'idle':
+            #             key.pause.trig = True
+            #             key.pause.status = 'pressed'
+            #     if event.key == pygame.K_r:
+            #         if key.restart.status == 'idle':
+            #             key.restart.trig = True
+            #             key.restart.status = 'pressed'
+                if event.key == pygame.K_RETURN:
+                    key.enter.status = 'pressed'
+                        
+            if event.type == pygame.KEYUP: #Keyboard keys release events
+                # if event.key == pygame.K_LEFT:
+                #     xChange += 1
+                # if event.key == pygame.K_RIGHT:
+                #     xChange += -1
+                # if event.key == pygame.K_DOWN:
+                #     key.down.status = 'released'
+                # if event.key == pygame.K_UP:
+                #     key.rotate.status = 'idle'
+                # if event.key == pygame.K_z:
+                #     key.cRotate.status = 'idle'
+                if event.key == pygame.K_p:
+                    key.pause.status = 'idle'
+                if event.key == pygame.K_r:
+                    key.restart.status = 'idle'
+                if event.key == pygame.K_RETURN:
+                    key.enter.status = 'idle'
         
         gameDisplay.fill(BLACK) #Whole screen is painted black in every iteration before any other drawings occur 
             
         mainBoard.gameAction() #Apply all the game actions here	
         mainBoard.draw() #Draw the new board after game the new game actions
         gameClock.update() #Increment the frame tick
-        
+        print('gameClock updated')
+        cv2.imshow("MediaPipe Hands", image)
         pygame.display.update() #Pygame display update		
         clock.tick(60) #Pygame clock tick function(60 fps)
     
